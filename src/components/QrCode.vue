51d43003c1ce57bd08e1faa64303d26c6c8163ad
ocbc-bb-devops <83942094+ocbc-bb-devops@users.noreply.github.com>
2021-09-15 11:39:03 +0800

    Add files via upload

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="qr-wrapper">
-    <vue-qrcode value="https://google.com" />
+    <form class="qr-code">
+      <div>
+        <input ref="product_code" type="text" placeholder="Product Code" />
+      </div>
+      <div>
+        <input ref="product_name" type="text" placeholder="Product Name" />
+      </div>
+      <div>
+        <input ref="card_number" type="text" placeholder="Card Number" />
+      </div>
+    </form>
+    <vue-qrcode
+      :value="url"
+      :options="{
+        errorCorrectionLevel: 'H',
+        width: 200
+      }"
+    />
+    <div class="qr-code--url">{{ url }}</div>
+    <div class="qr-code--deeplink">{{ deepLink }}</div>
+    <div>
+      <button @click="generateQR">{{ buttonText }}</button>
+    </div>
   </div>
 </template>
 
@@ -11,6 +33,34 @@ export default {
   name: 'QrCode',
   components: {
     VueQrcode
+  },
+  data() {
+    return {
+      url: 'https://google.com',
+      deepLink: '',
+      buttonText: 'Generate QR'
+    }
+  },
+  methods: {
+    generateQR() {
+      const productCode = this.$refs.product_code.value
+      const productName = this.$refs.product_name.value
+      const cardNumber = this.$refs.card_number.value
+      this.url = `https://google.com?productCode=${productCode}&productName=${productName}&cardNumber=${cardNumber}`
+      this.deepLink = `franksg://${productCode}/${productName}/${cardNumber}`
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.qr-code {
+  input {
+    margin-top: 0.5em;
+  }
+  &--url,
+  &--deeplink {
+    margin: 0 1em 1em;
+  }
+}
+</style>
